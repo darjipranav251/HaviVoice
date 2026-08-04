2023fd2f3c192feba7c7a0a0942652f3db4e6dbc
Pranav <darjipranav251@gmail.com>
2026-08-04 06:52:23 -0400

    fix(alembic): set down_revision of b41f7c9d2e05 to 0a44f84abfa2

--- api/alembic/versions/b41f7c9d2e05_migrate_gemini_2_5_flash_to_3_5_flash.py
+++ api/alembic/versions/b41f7c9d2e05_migrate_gemini_2_5_flash_to_3_5_flash.py
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "b41f7c9d2e05"
-down_revision: Union[str, None] = "00b0201ad918"
+down_revision: Union[str, None] = "0a44f84abfa2"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
